6c4d093562e4406c0bc6f701790ba4d1772e11e5
Nguyen Duc Anh <nguyenanh191025@gmail.com>
2025-10-27 00:48:16 +0700

    deploy

--- src/interfaces/invoices.type.ts
+++ src/interfaces/invoices.type.ts
@@ -1,9 +1,10 @@
+import { Types } from 'mongoose'
 import { STATUS_INVOICES } from '~/constants/enum'
 
 export interface IInvoice {
-  table_id?: number
-  user_id?: number
-  order_id?: number
+  table_id?: Types.ObjectId | string
+  user_id?: Types.ObjectId | string
+  order_id?: Types.ObjectId | string
   total_amount: number
   status: STATUS_INVOICES
   created_at?: string
